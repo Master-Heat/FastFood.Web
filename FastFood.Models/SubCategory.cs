ca2cabec9a31ad618a29c489e9c28d1ff0531bc9
Master <masterheaterheting@gmail.com>
2025-05-19 13:43:50 +0300

    Added pages for sub category

--- FastFood.Models/SubCategory.cs
+++ FastFood.Models/SubCategory.cs
@@ -7,9 +7,10 @@ namespace FastFood.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public String Title {  get; set; }
+        public string Title {  get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public ICollection<Item> Items { get; set; }
 
     }
 }
---
+++ FastFood.Web/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,93 @@
+﻿using FastFood.Models;
+using FastFood.Reposiory;
+using FastFood.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastFood.Web.Areas.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubCategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+           var  SubCategory = _context.SubCategories.Include(x => x.Category).ToList();
+            return View(SubCategory);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            SubCategoryViewModel vm = new SubCategoryViewModel();
+            ViewBag.category = new SelectList(_context.Categories, "Id", "Title");
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Create(SubCategoryViewModel vm)
+        {
+            SubCategory model = new SubCategory();
+            if (ModelState.IsValid) { 
+            
+            model.Title = vm.Title;
+            model.CategoryId = vm.CategoryId;
+            _context.SubCategories.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(vm);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            SubCategoryViewModel vm = new SubCategoryViewModel();
+            var subCategory = _context.SubCategories.Where(x => x.Id == id).FirstOrDefault();
+            if (subCategory != null) {
+                vm.Id = subCategory.Id;
+                vm.Title = subCategory.Title;
+                ViewBag.category = new SelectList(_context.Categories, "Id", "Title" ,subCategory.CategoryId);
+            }
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(SubCategoryViewModel vm)
+        {
+            SubCategory model = _context.SubCategories.Where(x => x.Id == vm.Id).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+
+                model.Title = vm.Title;
+                model.CategoryId = vm.CategoryId;
+                _context.SubCategories.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var subCategory = _context.SubCategories.Where(x => x.Id == id).FirstOrDefault();
+            if (subCategory != null)
+            {
+                _context.SubCategories.Remove(subCategory);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
